6e95ec24825e8d66be66f4f16b9b60c9b3e95714
Zdenek Mejzlik <zdenek.mejzlik@altworx.com>
2017-03-13 14:05:48 +0100

    Separate grok functionality from server functionality

--- src/groker.erl
+++ src/groker.erl
@@ -1,24 +1,22 @@
--module(groker).
+%%====================================================================
+%% Sample server demonstrating usage of groker module
 
-%-export([init/0, match/1]).
+-module(groker).
 
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, grok/1, get_pid/0]).
+-export([start_link/0, grok/1, syngrok/1, get_pid/0]).
 
 %% Gen_server_callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 %% Tests
--export([test1/0, test2/0, test3/0]).
+-export([match_test/0, benchmark1/0, benchmark2/0]).
 
--record(state, {patterns, metadata, start_time, count}).
+-record(state, {patterns, start_time, count}).
 
 -define(SERVER, ?MODULE).
--define(SPACE, 16#20).
--define(HASH, $#).
--define(BACKSLASH, $\\).
 -define(PATTERN_DIR, "./config/patterns/").
 -define(PATTERN_FILE, "./config/syslog_patterns").
 -define(LIMIT, 100000).
@@ -34,20 +32,17 @@ start_link() ->
 grok(Msg) -> 
     gen_server:cast(?SERVER, {grok, Msg}).
 
+syngrok(Msg) ->
+    gen_server:call(?SERVER, {grok, Msg}).
+
 get_pid() ->
     gen_server:call(?SERVER, getpid).
 
 %%--------------------------------------------------------------------
 init(_) ->
-    % U expanze vyrazu se pouziva hledani dle klicu, vhodna struktura je mapa
-    CorePatterns = load_patterns_from_dir(?PATTERN_DIR),
-    AppPatterns = load_patterns_from_file(?PATTERN_FILE),
-    Metadata = extract_metadata(AppPatterns),
-    ExpPatterns = maps:map(fun(_Key, Val) -> expand_pattern(Val, CorePatterns) end, AppPatterns),
-
-    % Pro zpracovani zprav se ale nehleda dle klicu ale iteruje se po vzorech, vhodnejsi je seznam
-    CompPatterns = maps:to_list(maps:map(fun(_Key, Val) -> compile_pattern(Val) end, ExpPatterns)),
-    {ok, #state{patterns = CompPatterns, metadata = Metadata, start_time = erlang:timestamp(), count = 0}}.
+    Patterns = groklib:get_patterns(?PATTERN_DIR, ?PATTERN_FILE),
+    % Patterns is a map but message processing doesn't use keys. Instead it iterates through patterns. List is better structure.
+    {ok, #state{patterns = maps:to_list(Patterns), start_time = erlang:timestamp(), count = 0}}.
 
 %%--------------------------------------------------------------------
 code_change(_OlvVsn, State, _Extra) ->
@@ -57,19 +52,17 @@ code_change(_OlvVsn, State, _Extra) ->
 handle_call(getpid, _From, State) ->
     {reply, self(), State};
 
+handle_call({grok, Msg}, _From,  #state{patterns = Patterns} = State) ->
+    Reply = groklib:match(Msg, Patterns),
+    {reply, Reply, State};
+
 handle_call(_Request, _From, State) ->
     Reply = unknown_request,
     {reply, Reply, State}.
 
 %%--------------------------------------------------------------------
-handle_cast({grok, Msg}, #state{patterns = Patterns, metadata = Metadata, start_time = StartTime, count = Count} = State) ->
-    case match(Msg, Patterns) of
-        nomatch ->
-            nomatch;
-        {PatternName, Data} ->
-            Output = create_output(PatternName, Data, Metadata)
-            %io:format("Data: ~p~n", [Output])
-    end,
+handle_cast({grok, Msg}, #state{patterns = Patterns, start_time = StartTime, count = Count} = State) ->
+    groklib:match(Msg, Patterns),
 
     NewCount = Count + 1,
     case NewCount =:= ?LIMIT of
@@ -93,265 +86,44 @@ handle_info(_Info, State) ->
 
 %%--------------------------------------------------------------------
 terminate(_Reason, _State) ->
-    ok.
+    io:format("Terminated~n", []).
 
 %%====================================================================
-%% Private functions
-
-%---------------------------------------------------------------------
-match(_Msg, []) ->
-    nomatch;
-
-match(Msg, [{Name, RE}|T]) ->
-    case re:run(Msg, RE, [global, {capture, all_but_first, list}]) of
-        {match, [Captured|_]} ->
-            {Name, Captured};
-        nomatch ->
-            match(Msg, T)
-    end.
+%% Tests (will be replaced by common tests)
 
 %%--------------------------------------------------------------------
-create_output(PatternName, Data, Metadata) ->
-    create_output1(Data, maps:get(PatternName, Metadata), #{}).
-
-create_output1([], [], Output) ->
-    Output;
-
-create_output1([Value|Data], [{Name, Type}|Metadata], Output) ->
-   create_output1(Data, Metadata, maps:put(Name, type(Type, Value), Output)).
+match_test() ->
+    io:format("~p~n", [syngrok("Gary is male, 25 years old and weighs 68.5 kilograms")]),
+    io:format("~p~n", [syngrok("Penny is female, 19 years old and weighs 54.0 kilograms")]),
+    io:format("~p~n", [syngrok("This message doesn't match")]).
 
 %%--------------------------------------------------------------------
-type(int, Val) ->
-    list_to_integer(Val);
-
-type(float, Val) ->
-    list_to_float(Val);
-
-type(_, Val) ->
-    Val.
-
-%%--------------------------------------------------------------------
-compile_pattern(P) ->
-    {ok, MP} = re:compile(P),
-    MP.
-
-%%--------------------------------------------------------------------
-extract_metadata(Patterns) ->
-    L = maps:to_list(Patterns),
-    M = extract_metadata(L, []),
-    maps:from_list(M).
-
-extract_metadata([], Metadata) ->
-    Metadata;
-
-extract_metadata([{Name, Pattern}|Patterns], Metadata) ->
-   Names = extract_names(Pattern),
-   Types = extract_types(Pattern),
-   Merged = merge_names_types(Names, Types),
-   extract_metadata(Patterns, [{Name, Merged}|Metadata]).
-
-%%--------------------------------------------------------------------
-extract_names(Pattern) ->
-    {match, Captured} = re:run(Pattern, "%{(\\w+):(\\w+)(?::\\w+)?}", [ungreedy, global, {capture,all_but_first,list}]),
-    lists:map(fun([_V, K | _]) -> {list_to_atom(K), undefined} end, Captured).
-
-%%--------------------------------------------------------------------
-extract_types(Pattern) ->
-    {match, Captured} = re:run(Pattern, "%{(\\w+):(\\w+):(\\w+)}", [ungreedy, global, {capture,all_but_first,list}]),
-    lists:map(fun([_V, K, T | _]) -> {list_to_atom(K), list_to_atom(T)} end, Captured).
-
-%%--------------------------------------------------------------------
-merge_names_types(Names, Types) ->
-    merge_names_types(Names, Types, []).
-
-merge_names_types([], _, Merged) ->
-    lists:reverse(Merged);
-
-merge_names_types([{Name, Type}|Names], Types, Merged) ->
-    T = case get_type(Name, Types) of 
-            undefined ->
-                Type;
-            Tp ->
-                Tp
-        end,
-    merge_names_types(Names, Types, [{Name, T}|Merged]).
-
-%%--------------------------------------------------------------------
-get_type(_, []) ->
-    undefined;
-
-get_type(Name, [{Name, Type}|_]) ->
-    Type;
-
-get_type(Name, [_|Types]) ->
-    get_type(Name, Types).
-
-%%--------------------------------------------------------------------
-expand_pattern(Pattern, Patterns) ->
-    %io:format("Entering high level expansion with ~p~n", [Pattern]),
-    Pattern1 = expand_high_level(Pattern, Patterns),
-
-    %io:format("Entering low level expansion with: ~p~n", [Pattern1]),
-    Pattern2 = expand_low_level(Pattern1, Patterns),
-
-    case re:run(Pattern2, "%{\\w+(:\\w+)?}", [ungreedy]) of 
-        nomatch ->
-            Pattern2;
-        {match, _} ->
-            expand_pattern(Pattern2, Patterns)
-    end.
-
-%%--------------------------------------------------------------------
-expand_high_level(Pattern, Patterns) ->
-    case re:run(Pattern, "%{(\\w+):(\\w+)(?::\\w+)?}", [ungreedy, {capture, all, list}]) of
-        nomatch -> 
-            Pattern;
-        {match, [String, Type, Name|_]} ->
-            Replacement = maps:get(Type, Patterns),
-            Replacement1 = escape("(?P<" ++ Name ++ ">" ++ Replacement ++ ")"),
-            %io:format("~p -> ~p~n", [Type, Replacement1]),
-            NewPattern = re:replace(Pattern, String, Replacement1, [ungreedy, {return, list}]),
-            %io:format("~p~n", [NewPattern]),
-            expand_high_level(NewPattern, Patterns)
-    end.
-
-%%--------------------------------------------------------------------
-expand_low_level(Pattern, Patterns) ->
-     case re:run(Pattern, "%{(\\w+)}", [ungreedy, {capture, all, list}]) of
-        nomatch -> 
-            Pattern;
-        {match, [String, Type|_]} ->
-            Replacement = maps:get(Type, Patterns),
-            Replacement1 = escape(Replacement),
-            %io:format("~p -> ~p~n", [Type, Replacement1]),
-            NewPattern = re:replace(Pattern, String, Replacement1, [ungreedy, {return, list}]),
-            %io:format("~p~n", [NewPattern]),
-            expand_low_level(NewPattern, Patterns)
-    end.
-
-%%--------------------------------------------------------------------
-escape(Str) ->
-    escape(Str, []).
-
-escape([], Rslt) ->
-    lists:reverse(Rslt);
-
-escape([H|T], Rslt) ->
-    case H =:= ?BACKSLASH of
-        true ->
-            escape(T, [H | [H | Rslt]]);
-        false ->
-            escape(T, [H | Rslt])
-    end.
-
-%%--------------------------------------------------------------------
-load_patterns_from_dir(Dir) ->
-    case file:list_dir(Dir) of
-        {ok, Files} ->
-            Paths = [lists:append(?PATTERN_DIR, F) || F <- Files],
-            load_patterns_from_files(Paths, #{});
-        _ ->
-            #{}
-    end.
-
-%%--------------------------------------------------------------------
-load_patterns_from_files([], Patterns) ->
-    Patterns;
-
-load_patterns_from_files([Path|Paths], Patterns) ->
-    NewPatterns = maps:merge(Patterns, load_patterns_from_file(Path)),
-    load_patterns_from_files(Paths, NewPatterns).
-
-%%--------------------------------------------------------------------
-load_patterns_from_file(Path) ->
-    {ok, File} = file:open(Path, [read]),
-    process_file(File).
-
-%%--------------------------------------------------------------------
-process_file(File) ->
-    process_file(File, #{}).
-
-process_file(File, Patterns) ->
-    case file:read_line(File) of
-        eof -> 
-            Patterns;
-        {ok, Line} ->
-            Rslt = process_line(Line),
-            case Rslt of
-                nopattern ->
-                    process_file(File, Patterns);
-                {pattern, Key, Val} ->
-                    NewPatterns = maps:put(Key, Val, Patterns),
-                    process_file(File, NewPatterns)
-            end
-    end.
-
-%%--------------------------------------------------------------------
-process_line(Line) ->
-    Line1 = string:strip(Line, right, $\n),
-    Line2 = string:strip(Line1),
-    case length(Line2) of
-        0 ->
-            % Empty line
-            nopattern;
-        _ ->
-            [F| _] = Line2,
-            case F of
-                ?HASH ->
-                    % Comment
-                    nopattern;
-                _ ->
-                    Pos = string:chr(Line2, ?SPACE),
-                    case Pos of
-                        0 ->
-                            % Invalid line
-                            nopattern;
-                        _ ->
-                            % Valid line
-                            Key = string:substr(Line2, 1, Pos - 1),
-                            Val = string:substr(Line2, Pos + 1),
-                            {pattern, string:strip(Key), string:strip(Val)}
-                    end
-            end
-    end.
-
-%%====================================================================
-%% Tests
-
-%%--------------------------------------------------------------------
-test1() ->
-    grok("gary is male, 25 years old and weighs 68.5 kilograms").
-
-%%--------------------------------------------------------------------
-test2() ->
+benchmark1() ->
     Pid = get_pid(),
-    test2(Pid).
+    benchmark1(Pid).
 
-test2(Pid) ->
+benchmark1(Pid) ->
     {message_queue_len, BoxLen} = process_info(Pid, message_queue_len),
 
     case BoxLen > 2000000 of
         true ->
             io:format("!~n"),
-            test2(Pid);
+            benchmark1(Pid);
         false ->
             grok("gary is male, 25 years old and weighs 68.5 kilograms"),
-            test2(Pid)
+            benchmark1(Pid)
     end.
 
 %%--------------------------------------------------------------------
-test3() ->
-    test3(0).
+benchmark2() ->
+    benchmark2(0).
 
-test3(Cnt) ->
-    %% Test 3 ma horsi vysleky nez test 2. Zda se, ze je pro celkovy vykon lepsi
-    %% kontinualne plnit mailbox nez ho precpat na zacatku a nechat byt
+benchmark2(Cnt) ->
     grok("gary is male, 25 years old and weighs 68.5 kilograms"),
     case Cnt > 300000 of
         true ->
             io:format("Fin~n", []);
         false ->
-            test3(Cnt + 1)
+            benchmark2(Cnt + 1)
     end.
 
- 
---
+++ src/groklib.erl
@@ -0,0 +1,241 @@
+-module(groklib).
+
+-export([get_patterns/2, match/2]).
+
+-define(SPACE, 16#20).
+-define(HASH, $#).
+-define(BACKSLASH, $\\).
+
+%%--------------------------------------------------------------------
+%% Receives text to match and list of compiled patterns.
+%% Returns either nomatch or tuple containing name of ther pattern 
+%% and list of captured data
+%%
+-spec match(Text :: list(), Patterns :: list(tuple())) -> nomatch | tuple().
+
+match(_Text, []) ->
+    nomatch;
+
+match(Text, [{Name, {Metadata, CompiledPattern}}|T]) ->
+    case re:run(Text, CompiledPattern, [global, {capture, all_but_first, list}]) of
+        {match, [Captured|_]} ->
+            {Name, convert_types(Captured, Metadata)};
+        nomatch ->
+            match(Text, T)
+    end.
+
+%%--------------------------------------------------------------------
+%% Returns list of compiled app patterns with their names and metadata
+%%
+get_patterns(CorePatternDir, AppPatternFile) ->
+    CorePatterns = load_patterns_from_dir(CorePatternDir),
+    AppPatterns = load_patterns_from_file(AppPatternFile),
+    Metadata = extract_metadata(AppPatterns),
+    ExpandedPatterns = maps:map(fun(Key, Val) -> {maps:get(Key, Metadata), expand_pattern(Val, CorePatterns)} end, AppPatterns),
+    maps:map(fun(_Key, {PatternMetadata, ExpandedPattern}) -> {PatternMetadata, compile_pattern(ExpandedPattern)} end, ExpandedPatterns).
+
+%%--------------------------------------------------------------------
+expand_pattern(Pattern, Patterns) ->
+    %io:format("Entering high level expansion with ~p~n", [Pattern]),
+    Pattern1 = expand_high_level(Pattern, Patterns),
+
+    %io:format("Entering low level expansion with: ~p~n", [Pattern1]),
+    Pattern2 = expand_low_level(Pattern1, Patterns),
+
+    case re:run(Pattern2, "%{\\w+(:\\w+)?}", [ungreedy]) of 
+        nomatch ->
+            Pattern2;
+        {match, _} ->
+            expand_pattern(Pattern2, Patterns)
+    end.
+
+%%--------------------------------------------------------------------
+expand_high_level(Pattern, Patterns) ->
+    case re:run(Pattern, "%{(\\w+):(\\w+)(?::\\w+)?}", [ungreedy, {capture, all, list}]) of
+        nomatch -> 
+            Pattern;
+        {match, [String, Type, Name|_]} ->
+            Replacement = maps:get(Type, Patterns),
+            Replacement1 = escape("(?P<" ++ Name ++ ">" ++ Replacement ++ ")"),
+            %io:format("~p -> ~p~n", [Type, Replacement1]),
+            NewPattern = re:replace(Pattern, String, Replacement1, [ungreedy, {return, list}]),
+            %io:format("~p~n", [NewPattern]),
+            expand_high_level(NewPattern, Patterns)
+    end.
+
+%%--------------------------------------------------------------------
+expand_low_level(Pattern, Patterns) ->
+     case re:run(Pattern, "%{(\\w+)}", [ungreedy, {capture, all, list}]) of
+        nomatch -> 
+            Pattern;
+        {match, [String, Type|_]} ->
+            Replacement = maps:get(Type, Patterns),
+            Replacement1 = escape(Replacement),
+            %io:format("~p -> ~p~n", [Type, Replacement1]),
+            NewPattern = re:replace(Pattern, String, Replacement1, [ungreedy, {return, list}]),
+            %io:format("~p~n", [NewPattern]),
+            expand_low_level(NewPattern, Patterns)
+    end.
+
+%%--------------------------------------------------------------------
+load_patterns_from_dir(Dir) ->
+    case file:list_dir(Dir) of
+        {ok, Files} ->
+            Paths = [lists:append(Dir, F) || F <- Files],
+            load_patterns_from_files(Paths, #{});
+        _ ->
+            #{}
+    end.
+
+%%--------------------------------------------------------------------
+load_patterns_from_files([], Patterns) ->
+    Patterns;
+
+load_patterns_from_files([Path|Paths], Patterns) ->
+    NewPatterns = maps:merge(Patterns, load_patterns_from_file(Path)),
+    load_patterns_from_files(Paths, NewPatterns).
+
+%%--------------------------------------------------------------------
+load_patterns_from_file(Path) ->
+    {ok, File} = file:open(Path, [read]),
+    process_file(File).
+
+%%--------------------------------------------------------------------
+process_file(File) ->
+    process_file(File, #{}).
+
+process_file(File, Patterns) ->
+    case file:read_line(File) of
+        eof -> 
+            Patterns;
+        {ok, Line} ->
+            Rslt = process_line(Line),
+            case Rslt of
+                nopattern ->
+                    process_file(File, Patterns);
+                {pattern, Key, Val} ->
+                    NewPatterns = maps:put(Key, Val, Patterns),
+                    process_file(File, NewPatterns)
+            end
+    end.
+
+%%--------------------------------------------------------------------
+process_line(Line) ->
+    Line1 = string:strip(Line, right, $\n),
+    Line2 = string:strip(Line1),
+    case length(Line2) of
+        0 ->
+            % Empty line
+            nopattern;
+        _ ->
+            [F| _] = Line2,
+            case F of
+                ?HASH ->
+                    % Comment
+                    nopattern;
+                _ ->
+                    Pos = string:chr(Line2, ?SPACE),
+                    case Pos of
+                        0 ->
+                            % Invalid line
+                            nopattern;
+                        _ ->
+                            % Valid line
+                            Key = string:substr(Line2, 1, Pos - 1),
+                            Val = string:substr(Line2, Pos + 1),
+                            {pattern, string:strip(Key), string:strip(Val)}
+                    end
+            end
+    end.
+
+%%--------------------------------------------------------------------
+convert_types(Data, Metadata) ->
+    convert_types(Data, Metadata, #{}).
+
+convert_types([], [], Result) ->
+    Result;
+
+convert_types([Value|Data], [{Name, Type}|Metadata], Result) ->
+   convert_types(Data, Metadata, maps:put(Name, convert_type(Type, Value), Result)).
+
+%%--------------------------------------------------------------------
+convert_type(int, Val) ->
+    list_to_integer(Val);
+
+convert_type(float, Val) ->
+    list_to_float(Val);
+
+convert_type(_, Val) ->
+    Val.
+
+%%--------------------------------------------------------------------
+compile_pattern(P) ->
+    {ok, MP} = re:compile(P),
+    MP.
+
+%%--------------------------------------------------------------------
+extract_metadata(Patterns) ->
+    L = maps:to_list(Patterns),
+    M = extract_metadata(L, []),
+    maps:from_list(M).
+
+extract_metadata([], Metadata) ->
+    Metadata;
+
+extract_metadata([{Name, Pattern}|Patterns], Metadata) ->
+   Names = extract_names(Pattern),
+   Types = extract_types(Pattern),
+   Merged = merge_names_types(Names, Types),
+   extract_metadata(Patterns, [{Name, Merged}|Metadata]).
+
+%%--------------------------------------------------------------------
+extract_names(Pattern) ->
+    {match, Captured} = re:run(Pattern, "%{(\\w+):(\\w+)(?::\\w+)?}", [ungreedy, global, {capture,all_but_first,list}]),
+    lists:map(fun([_V, K | _]) -> {list_to_atom(K), undefined} end, Captured).
+
+%%--------------------------------------------------------------------
+extract_types(Pattern) ->
+    {match, Captured} = re:run(Pattern, "%{(\\w+):(\\w+):(\\w+)}", [ungreedy, global, {capture,all_but_first,list}]),
+    lists:map(fun([_V, K, T | _]) -> {list_to_atom(K), list_to_atom(T)} end, Captured).
+
+%%--------------------------------------------------------------------
+merge_names_types(Names, Types) ->
+    merge_names_types(Names, Types, []).
+
+merge_names_types([], _, Merged) ->
+    lists:reverse(Merged);
+
+merge_names_types([{Name, Type}|Names], Types, Merged) ->
+    T = case get_type(Name, Types) of 
+            undefined ->
+                Type;
+            Tp ->
+                Tp
+        end,
+    merge_names_types(Names, Types, [{Name, T}|Merged]).
+
+%%--------------------------------------------------------------------
+get_type(_, []) ->
+    undefined;
+
+get_type(Name, [{Name, Type}|_]) ->
+    Type;
+
+get_type(Name, [_|Types]) ->
+    get_type(Name, Types).
+
+%%--------------------------------------------------------------------
+escape(Str) ->
+    escape(Str, []).
+
+escape([], Rslt) ->
+    lists:reverse(Rslt);
+
+escape([H|T], Rslt) ->
+    case H =:= ?BACKSLASH of
+        true ->
+            escape(T, [H | [H | Rslt]]);
+        false ->
+            escape(T, [H | Rslt])
+    end.
+
